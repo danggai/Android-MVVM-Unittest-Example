683c3bd61f23d9fb84a71556fcc6028904ca506b
danggai <donggi9313@naver.com>
2020-12-27 19:33:21 +0900

    - Create 2nd button : use view id to separate button type in viewmodel

--- app/src/main/java/com/example/android_mvvm_unittest_example/ui/main/MainFragViewModel.kt
+++ app/src/main/java/com/example/android_mvvm_unittest_example/ui/main/MainFragViewModel.kt
@@ -2,6 +2,7 @@ package com.example.android_mvvm_unittest_example.ui.main
 
 import android.app.Application
 import android.util.Log
+import android.view.View
 import androidx.lifecycle.MutableLiveData
 import com.example.android_mvvm_unittest_example.R
 import com.example.android_mvvm_unittest_example.ui.BaseViewModel
@@ -9,16 +10,31 @@ import com.example.android_mvvm_unittest_example.ui.BaseViewModel
 class MainFragViewModel(override val app: Application) : BaseViewModel(app) {
 
     private var clickTime: Int = 0
-
-    private var clickTimeDesc: MutableLiveData<String> = MutableLiveData<String>(String.format(getString(R.string.click_count_desc), 0))
+    private var clickTimeDesc: MutableLiveData<String> = MutableLiveData<String>(String.format(getString(R.string.click_count_desc), clickTime))
     val _clickTimeDesc: MutableLiveData<String> = clickTimeDesc
 
+    private var remainTime: Int = 10
+    private var remainTimeDesc: MutableLiveData<String> = MutableLiveData<String>(String.format(getString(R.string.remain_count_desc), remainTime))
+    val _remainTimeDesc: MutableLiveData<String> = remainTimeDesc
 
-    fun onClick() {
-        Log.d("btnClick", "user clicked button.")
-        clickTime += 1
 
-        clickTimeDesc.value = String.format(getString(R.string.click_count_desc), clickTime)
+    fun onClick(v: View) {
+        when (v.id) {
+            R.id.btn_first -> {
+                Log.d("btnClick", "button first")
+                clickTime += 1
+                clickTimeDesc.value = String.format(getString(R.string.click_count_desc), clickTime)
+            }
+            R.id.btn_second -> {
+                Log.d("btnClick", "button second")
+                _remainTimeDesc.value = if (remainTime > 0) {
+                    remainTime -= 1
+                    String.format(getString(R.string.remain_count_desc), remainTime)
+                } else {
+                    getString(R.string.remain_count_desc_fake)
+                }
+            }
+        }
     }
 
 }
