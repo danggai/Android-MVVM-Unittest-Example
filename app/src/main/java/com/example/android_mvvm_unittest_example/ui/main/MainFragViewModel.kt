ae533e0948a838bf04e0ba14a3811f29d0e6a4c2
danggai <donggi9313@naver.com>
2020-12-27 18:58:00 +0900

    - Basic button click event Created.

--- app/src/main/java/com/example/android_mvvm_unittest_example/ui/main/MainFragViewModel.kt
+++ app/src/main/java/com/example/android_mvvm_unittest_example/ui/main/MainFragViewModel.kt
@@ -1,13 +1,24 @@
 package com.example.android_mvvm_unittest_example.ui.main
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.example.android_mvvm_unittest_example.R
 import com.example.android_mvvm_unittest_example.ui.BaseViewModel
 
 class MainFragViewModel(override val app: Application) : BaseViewModel(app) {
 
-    private var mainStr: MutableLiveData<String> = MutableLiveData<String>(getString(R.string.string_default))
-    val _mainStr: MutableLiveData<String> = mainStr
+    private var clickTime: Int = 0
+
+    private var clickTimeDesc: MutableLiveData<String> = MutableLiveData<String>(String.format(getString(R.string.click_count_desc), 0))
+    val _clickTimeDesc: MutableLiveData<String> = clickTimeDesc
+
+
+    fun onClick() {
+        Log.d("btnClick", "user clicked button.")
+        clickTime += 1
+
+        clickTimeDesc.value = String.format(getString(R.string.click_count_desc), clickTime)
+    }
 
 }
