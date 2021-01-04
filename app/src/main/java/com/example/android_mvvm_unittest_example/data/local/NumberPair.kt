4af07e673e6808ceca927dc7b8e2a1bb711ea588
danggai <donggi9313@naver.com>
2021-01-04 16:57:28 +0900

    - Test modified with mockito.

---
+++ app/src/main/java/com/example/android_mvvm_unittest_example/data/local/NumberPair.kt
@@ -0,0 +1,6 @@
+package com.example.android_mvvm_unittest_example.data.local
+
+data class NumberPair(
+        val origin: Int,
+        val add: Int
+)
--- app/src/test/java/com/example/android_mvvm_unittest_example/util/NumAddTest.kt
+++ app/src/test/java/com/example/android_mvvm_unittest_example/util/NumAddTest.kt
@@ -1,7 +1,9 @@
 package com.example.android_mvvm_unittest_example.util
 
-import android.util.Log
+import com.example.android_mvvm_unittest_example.data.local.NumberPair
+import org.mockito.Mockito.*
 import org.hamcrest.CoreMatchers
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Assert
 import org.junit.Test
 
@@ -9,21 +11,32 @@ class NumAddTest {
     @Test
     fun test_1() {
         val result: Int = 2 + 2
-        val value = CommonFunction.numAdd(2,2)
+        val value = CommonFunction.numAdd(2, 2)
         Assert.assertThat(result, CoreMatchers.`is`(value))
     }
 
     @Test
     fun test_2() {
         val result: Int = 8 + (-1)
-        val value = CommonFunction.numAdd(8,-1)
+        val value = CommonFunction.numAdd(8, -1)
         Assert.assertThat(result, CoreMatchers.`is`(value))
     }
 
     @Test
     fun test_3() {
         val result: Int = 65535 + 1
-        val value = CommonFunction.numAdd(65535,1)
+        val value = CommonFunction.numAdd(65535, 1)
         Assert.assertThat(result, CoreMatchers.`is`(value))
+    }
+
+    @Test
+    fun test_mockito_1() {
+        val mockedPair = mock(NumberPair::class.java)
+
+        `when`(mockedPair.origin).thenReturn(3)
+        `when`(mockedPair.add).thenReturn(2)
+
+        assertThat(3, CoreMatchers.`is`(mockedPair.origin))
+        assertThat(2, CoreMatchers.`is`(mockedPair.add))
     }
 }
